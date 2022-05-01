fbdf4a749e1812a0ced1d9e50d9f7d8cc4201be5
Saxda <githubrm@gmail.com>
2022-05-01 13:56:41 +0200

    changes

---
+++ RandomNumberGenerator/RandomNumberGenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to RandomNumberGenerator!")
+print("Please indicate in which area the number/numbers should be generated and how many numbers should be generated.")
+print("Please note that you first enter the number range and then the number of numbers.")
+print("Please note that you only use whole numbers.")
+
+number_range = int(input())
+number_of_numbers = int(input())
--- main.py
+++ main.py
@@ -1,2 +1,1 @@
 print("Welcome to generators")
-print(12)
