5569b66b127e81e6ce3a47cf02fba884990ec22c
Saxda <96083920+Saxda@users.noreply.github.com>
2022-05-18 22:30:44 +0200

    changes

--- Password-Generator/Password-Generator.py
+++ Password-Generator/Password-Generator.py
@@ -1,11 +1,11 @@
 print("Welcome to Password-Generator")
+print("Special characters are: !, \", §, %, &, /, (, ), =, ?, # and *")
 
-number_of_characters = int(input("Enter the length of the password"))
 
-
+number_of_characters = int(input("Enter the length of the password: "))
 
 import random
 
 for num in range(number_of_characters):
-	num = random.randint(character_range_a, character_range_b)
+	num = random.randint(0, 50)
 	print(num)
