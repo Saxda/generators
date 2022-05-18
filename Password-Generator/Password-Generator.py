4ae1effe4d35a7d89b3f8022f881037e528ea7c1
Saxda <96083920+Saxda@users.noreply.github.com>
2022-05-18 22:20:24 +0200

    changes

--- Password-Generator/Password-Generator.py
+++ Password-Generator/Password-Generator.py
@@ -1,1 +1,11 @@
 print("Welcome to Password-Generator")
+
+number_of_characters = int(input("Enter the length of the password"))
+
+
+
+import random
+
+for num in range(number_of_characters):
+	num = random.randint(character_range_a, character_range_b)
+	print(num)
